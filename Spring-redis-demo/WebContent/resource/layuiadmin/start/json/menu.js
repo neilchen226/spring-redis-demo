de5ca26cd4ec132c782c6fbec6f0df1338d1dca3
neilchen226 <157817985@qq.com>
2018-09-13 18:16:49 +0800

    init2

---
+++ Spring-redis-demo/WebContent/resource/layuiadmin/start/json/menu.js
@@ -0,0 +1,101 @@
+{
+    "code": 0,
+    "msg": "",
+    "data": [{
+        "title": "主页",
+        "icon": "layui-icon-home",
+        "list": [{
+            "title": "控制台",
+            "jump": "phsqms/home"
+        }]
+    },
+    {
+        "name": "",
+        "title": "基础数据",
+        "icon": "layui-icon-user",
+        "list": [{
+            "title": "地点",
+            "jump": "phsqms/phsq/locationList"
+        },
+        {
+            "title": "地点对应",
+            "jump": "phsqms/phsq/locationExtList"
+        },
+        {
+            "title": "币种",
+            "jump": "phsqms/phsq/ccyList"
+        },
+        {
+            "title": "特色",
+            "jump": "phsqms/phsq/specialList"
+        },
+        {
+            "title": "特色对应",
+            "jump": "phsqms/phsq/ptypeSpecialList"
+        }]
+    },
+    {
+    	"name": "",
+        "title": "数据统计",
+        "icon": "layui-icon-chart-screen",
+        "list": [{
+            "title": "用户统计",
+            "jump": "/phsqms/phsq/userCountList"
+        },{
+            "title": "订单统计",
+            "jump": "/phsqms/phsq/phOrderList"
+        }]
+    },
+    {
+        "name": "",
+        "title": "系统管理",
+        "icon": "layui-icon-set",
+        "list": [{
+            "name": "",
+            "title": "系统用户",
+            "spread": true,
+            "list": [{
+                "name": "",
+                "title": "用户管理"
+            },
+            {
+                "name": "",
+                "title": "角色管理"
+            }]
+        },
+        {
+            "name": "",
+            "title": "微信设置",
+            "spread": true,
+            "list": [{
+                "name": "",
+                "title": "菜单设置",
+                "jump": ""
+            },
+            {
+                "name": "",
+                "title": "关注回复",
+                "jump": ""
+            },
+            {
+                "name": "",
+                "title": "关键字回复",
+                "jump": ""
+            },
+            {
+                "name": "",
+                "title": "素材管理",
+                "jump": ""
+            }]
+        },
+        {
+            "name": "",
+            "title": "系统设置",
+            "spread": true,
+            "list": [{
+                "name": "info",
+                "title": "基本设置"
+            }]
+        }]
+    }]
+}
